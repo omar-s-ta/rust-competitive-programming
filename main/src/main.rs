d905623855419d87e555963af62ddca20b8c5736
Omar Saber Talaat Abdelrahman <omar.abdelrahman.de@gmail.com>
2025-09-17 15:22:29 +0200

    clear auto generated main

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,6 +1,5 @@
-// https://open.kattis.com/problems/pairingsocks
+
 pub mod solution {
-//{"name":"Pairing Socks","group":"Kattis","url":"https://open.kattis.com/problems/pairingsocks","interactive":false,"timeLimit":1000,"tests":[{"input":"2\n1 2 2 1\n","output":"4\n"},{"input":"1\n3 7\n","output":"impossible\n"},{"input":"3\n5 5 5 5 5 5\n","output":"6\n"}],"testType":"single","input":{"type":"stdin","fileName":null,"pattern":null},"output":{"type":"stdout","fileName":null,"pattern":null},"languages":{"java":{"taskClass":"PairingSocks"}}}
 
 use crate::algo_lib::io::input::Input;
 use crate::algo_lib::io::output::Output;
@@ -11,31 +10,6 @@ use crate::algo_lib::misc::test_type::TestType;
 type PreCalc = ();
 
 fn solve(input: &mut Input, out: &mut Output, _test_case: usize, _data: &mut PreCalc) {
-    let n = input.read_size();
-    let mut stk = input.read_vec::<i32>(2 * n);
-    stk.reverse();
-
-    let mut aux_stk = Vec::new();
-    let mut result = 0;
-
-    while let Some(ai) = stk.pop() {
-        if let Some(&aai) = aux_stk.last() {
-            if ai == aai {
-                aux_stk.pop();
-            } else {
-                aux_stk.push(ai);
-            }
-        } else {
-            aux_stk.push(ai);
-        }
-        result += 1;
-    }
-
-    if aux_stk.is_empty() {
-        out.print_line(result);
-    } else {
-        out.print_line("impossible");
-    }
 }
 
 pub static TEST_TYPE: TestType = TestType::Single;
@@ -68,9 +42,8 @@ pub(crate) fn run(mut input: Input, mut output: Output) -> bool {
         TaskType::Interactive => true,
     }
 }
-
-
 }
+
 pub mod algo_lib {
 #![allow(clippy::too_many_arguments)]
 #![allow(clippy::type_complexity)]
